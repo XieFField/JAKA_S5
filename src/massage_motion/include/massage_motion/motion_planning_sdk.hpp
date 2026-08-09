57e7f538b331c294c8dc58f47dcebe699a4c05a0
XieFField <3432674139@qq.com>
2026-08-09 23:05:49 +0800

    sdk

--- src/massage_motion/include/massage_motion/motion_planning_sdk.hpp
+++ src/massage_motion/include/massage_motion/motion_planning_sdk.hpp
@@ -1,9 +1,32 @@
 #ifndef MOTION_PLANNING_SDK_HPP_
 #define MOTION_PLANNING_SDK_HPP_
 
+#include <map>
+#include <memory>
+
+#include "massage_motion/motion_types.hpp"
+#include "massage_motion/lin_planner.hpp"
+#include "massage_motion/circ_planner.hpp"
+#include "massage_motion/ptp_planner.hpp"
+#include "massage_motion/moveit_planner_base.hpp"
+
 namespace massage_motion
 {
 
+
+class MotionPlanningSdk final : public IMotionPlanner
+{
+public:
+    MotionPlanningSdk(
+        const rclcpp::Node::SharedPtr & node,
+        const PlannerConfig & planner_config);
+
+    PlanResult plan(const MotionRequest & request) override;
+protected:
+
+private:
+    std::map<MotionType, std::shared_ptr<IMotionPlanner>> planners_;
+};
 
 } // namespace massage_motion
 
---
+++ src/massage_motion/src/motion_planning_sdk.cpp
@@ -0,0 +1,45 @@
+#include "massage_motion/motion_planning_sdk.hpp"
+
+namespace massage_motion
+{
+
+MotionPlanningSdk::MotionPlanningSdk(
+        const rclcpp::Node::SharedPtr & node,
+        const PlannerConfig & planner_config)
+{
+    planners_.emplace(
+        MotionType::kPtp,
+        std::make_unique<PtpPlanner>(node, planner_config)
+    );
+
+    planners_.emplace(
+        MotionType::kLin,
+        std::make_unique<LinPlanner>(node, planner_config)
+    );
+
+    planners_.emplace(
+        MotionType::kCirc,
+        std::make_unique<CircPlanner>(node, planner_config)
+    );
+}
+
+PlanResult MotionPlanningSdk::plan(const MotionRequest & request)
+{
+    const auto iterator = planners_.find(request.motion_type);
+    if (iterator == planners_.end() || !iterator->second)
+    {
+        return {
+            false,
+            MotionError::kUnsupportedMotion,
+            0,
+            "没有注册对应动作类型的规划器",
+            moveit_msgs::msg::RobotTrajectory{},
+            0.0,
+            ""
+        };
+    }
+
+    return iterator->second->plan(request);
+}
+
+}
